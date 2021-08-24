69115d8282924b7ac109d1b2d2923cebba816427
Ciara <ciarazsnijders@gmail.com>
2021-08-24 21:24:49 +0200

    error handling middleware

--- index.js
+++ index.js
@@ -80,3 +80,8 @@ app.use(express.static('public'));
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
+//error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
