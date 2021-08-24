298232e9ce08c57b3bb2f44ff3fe0f884a8f6265
Ciara <ciarazsnijders@gmail.com>
2021-08-24 21:26:04 +0200

    added spacing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
   morgan = require('morgan');
 
 const app = express();
+
 //Morgan middleware library to log all requests 
 app.use(morgan('common'));
 
@@ -80,12 +81,15 @@ app.get('/', (req, res) => {
 app.get('/movies', (req, res) => {
   res.json(movies);
 });
+
 //express.static to serve your “documentation.html” file from the public folder 
 app.use(express.static('public'));
+
 // listen for requests
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
+
 //error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
