b759b192eb7cae27272404df5279fb7595705625
Ciara <ciarazsnijders@gmail.com>
2021-09-25 12:54:43 +0200

    create models file and populated with the Mongoose schemas

---
+++ models.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    year: String,
+    genre:  String,
+    director: {
+        name: String,
+        bio: String,
+        birthYear: Date,
+        deathYear: Date,
+    },
+    imageURL: String, 
+    featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    Favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let genreSchema = mongoose.Schema({
+    Name: String,
+    Description: String
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Genre = Genre;
